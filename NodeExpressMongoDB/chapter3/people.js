9c774b7c0d30ac733d89bc12f237e702f8574edb
Soumodip Das <soumodipd567@gmail.com>
2023-01-25 15:49:36 +0530

    modules

---
+++ NodeExpressMongoDB/chapter3/people.js
@@ -0,0 +1,8 @@
+const people = ['wiz', 'pekka', 'vulk'];
+const ages = [23, 24, 25];
+console.log(people);
+module.exports = 'hello';
+module.exports = people;
+module.exports = {
+    people, ages
+};
