4402381aa1729d45e76050a83b8f8950169127ba
Soumodip Das <soumodipd567@gmail.com>
2023-01-31 10:38:13 +0530

    ok

---
+++ NodeExpressMongoDB/chapter4.js
@@ -0,0 +1,24 @@
+//file system
+
+const fs = require('fs');
+
+//reading files
+// fs.readFile('./docs/doc1.txt', (err, data) => {
+//    if(err){
+//     console.log(error);
+//    } else {
+//     console.log(data.toString());
+//    }
+// });
+
+// console.log('last line');
+
+//writing files
+23.41
+
+
+
+//directories
+
+//deleting files
+
