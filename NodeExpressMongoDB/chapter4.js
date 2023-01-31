52523471779f2e597ca6faa5c72f5273229229f3
Soumodip Das <soumodipd567@gmail.com>
2023-01-31 10:59:54 +0530

    committed

--- NodeExpressMongoDB/chapter4.js
+++ NodeExpressMongoDB/chapter4.js
@@ -13,12 +13,44 @@ const fs = require('fs');
 
 // console.log('last line');
 
+
+
 //writing files
-23.41
+// fs.writeFile('./docs/doc1.txt', 'hello, pekka', () => {
+//    console.log('file was written');
+// });
+
+// fs.writeFile('./docs/doc2.txt', 'hello, pekka', () => {
+//     console.log('file was written');
+// });
 
 
 
 //directories
+// if(!fs.existsSync('./assets')){
+//     fs.mkdir('./assets', (err) => {
+//         if(err) {
+//             console.log(err);
+//         }
+//         console.log('folder created');
+//     });
+// } else {
+//     fs.rmdir('./assets', (err) => {
+//         if(err) {
+//             console.log(err);
+//         }
+//         console.log('folder deleted');
+//     });
+// }
+
+
 
 //deleting files
-
+if(fs.existSyns('./docs/deleteMe.txt')) {
+    fs.unlink('./docs/deleteMe.txt', (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('file deleted');
+    })
+}
