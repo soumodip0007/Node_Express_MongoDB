7d46afcdd013e81558a5dc4d67c62668068214c6
Soumodip Das <soumodipd567@gmail.com>
2023-02-10 15:32:07 +0530

    ok

--- Express/enginesViews.js
+++ Express/enginesViews.js
@@ -13,20 +13,25 @@ app.listen(3000);
 app.get('/', (req, res) => {
   //send method instead of write and end method 
   //res.send('<p>home page</p>');
-  res.render('index');
+  const tasks = [
+        { title: 'learn html', body: 'task', assignedBy: 'faculty - Jhon', id: '1' },
+        { title: 'learn css', body: 'task', assignedBy: 'faculty - Kevin', id: '2' },
+        { title: 'learn js', body: 'task', assignedBy: 'faculty - Joseph', id: '3' },
+    ];
+  res.render('index', {title: 'Home', tasks});
 });
 
 app.get('/about', (req, res) => {
     //send method instead of write and end method 
     //res.send('<p>about page</p>');
-    res.render('about');
+    res.render('about', {title: 'About'});
 });
 
 app.get('/tasks/create', (req, res) => {
-    res.render('create');
+    res.render('create', {title: 'Create a new blog'});
 })
 
 //404 page
 app.use((req, res) => {
-    res.status(404).render('404')
+    res.status(404).render('404', {title: '404 - Not Found'});
 });
