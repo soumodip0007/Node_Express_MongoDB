dd1e32fb0046bdc402017fc2c302567bfd02bb57
Soumodip Das <soumodipd567@gmail.com>
2023-02-16 10:52:35 +0530

    committed

--- Express/enginesViewsAndMiddleware.js
+++ Express/enginesViewsAndMiddleware.js
@@ -1,4 +1,6 @@
 const express = require('express');
+// third party middleware
+const morgan = require('morgan')
 
 //express app
 
@@ -9,6 +11,25 @@ app.set('view engine', 'ejs');
 
 //listen for request
 app.listen(3000);
+
+// third party middleware
+app.use(morgan('dev'));
+// app.use(morgan('tiny'));
+
+//Middleware
+// app.use((req, res, next) => {
+//     console.log('new request made:');
+//     console.log('host: ', req.hostname);
+//     console.log('path: ', req.path);
+//     console.log('method: ', req.method);
+//     next();
+// });
+
+//Middleware
+// app.use((req, res, next) => {
+//     console.log('in the next middleware');
+//     next();
+// });
 
 app.get('/', (req, res) => {
   //send method instead of write and end method 
@@ -26,6 +47,7 @@ app.get('/about', (req, res) => {
     //res.send('<p>about page</p>');
     res.render('about', {title: 'About'});
 });
+
 
 app.get('/tasks/create', (req, res) => {
     res.render('create', {title: 'Create a new blog'});
